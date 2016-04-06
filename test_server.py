8742133c52121bb20f48ddaf75a8c761b12e79e0
Joel Gugger <joel@thimoo.ch>
2016-04-06 16:03:24 +0200

    clean tests

--- test_server.py
+++ test_server.py
@@ -64,7 +64,7 @@ def encrypt_data_wrong_mac (timestamp, data):
     to_pad = 16 - (len(data) % 16)
     plaintext = data.encode() + struct.pack('B', to_pad) * to_pad
     ciphertext = AES.new (client.KEY_E, AES.MODE_CBC, IV).encrypt (plaintext)
-    tag = HMAC.new (client.KEY_A, plaintext, SHA256).digest()[:8] + b'\x00\x00'
+    tag = HMAC.new (client.KEY_A, data.encode(), SHA256).digest()[:10] # hash without the padding
 
     return b'\x00' + timestamp + ciphertext + tag
 
@@ -73,17 +73,13 @@ def encrypt_data_wrong_mac (timestamp, data):
 if __name__ == '__main__' :
   
   t = int(time.time())
-  print("\ntest for timestamp : " + str(t))
+  # print("\ntest for timestamp : " + str(t))
   print("Result expected : OK")
-  
   client.main(t, 'toto')
   
-  print("\nre-test for timestamp : " + str(t))
-  print("Result expected : WRONG TSP")
-  
+  # print("\nre-test for timestamp : " + str(t))
+  print("\nResult expected : WRONG TSP")
   client.main(t, 'toto')
-  
-  
   
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   
@@ -91,23 +87,22 @@ if __name__ == '__main__' :
       # Connect to server and send data
       sock.connect((client.HOST, client.PORT))
 
-      print("\ntest for padding : ")
-      print("timestamp : " + str(int(t)+4))
-      print("Result expected : PAD ERROR")
+      #print("\ntest for padding : ")
+      #print("timestamp : " + str(int(t)+4))
+      print("\nResult expected : PADDING ERROR")
       timestamp = struct.pack('>q', t+4)
       sock.sendall (encrypt_data_wrong_pad (timestamp, 'datadata'))
 
       client.parse_answer (t+4, sock.recv(35))
       
-      print("\ntest for mac : ")
-      print("timestamp : " + str(int(t)+5))
-      print("Result expected : MAC ERROR")
+      #print("\ntest for mac : ")
+      #print("timestamp : " + str(int(t)+5))
+      print("\nResult expected : MAC ERROR")
       
       timestamp = struct.pack('>q', t+6)
       crypt = encrypt_data_wrong_mac(timestamp, 'data')
-      print(crypt)
       sock.sendall (crypt)
-      client.parse_answer (t+5, sock.recv(35))
+      client.parse_answer (t+6, sock.recv(35))
       
   finally:
       sock.close()
