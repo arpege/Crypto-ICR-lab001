bb2a6e78dbb06b0674fd8c49af8e059a29831828
Joel Gugger <joel@thimoo.ch>
2016-04-06 16:08:16 +0200

    clean header

--- test_server.py
+++ test_server.py
@@ -2,30 +2,6 @@
 
 # The MIT License (MIT)
 # Copyright (c) 2016 Joel Gugger <joel.gugger@master.hes-so.ch>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is furnished
-# to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-# Version 1.0 (24-03-2016)
-#
-# Write a test suite in the programming language of your choice allowing to check 
-# that both the client and the server security functionalities behave as expected, 
-# including in case of error.
 # 
 # This is the test suite for the server side
 
